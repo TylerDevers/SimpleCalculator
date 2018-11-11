5065e038216969ee9eb718370688d210a72f7907
Tyler <devers.tyler@gmail.com>
2018-11-11 10:44:19 -0800

    updated notes

--- simpleCalculator/simpleCalculator/simpleCalculator/Program.cs
+++ simpleCalculator/simpleCalculator/simpleCalculator/Program.cs
@@ -6,7 +6,7 @@ namespace simpleCalculator
     {
         static void Main(string[] args)
         {
-            Person person1 = new Person(); // testing Person Class
+            Person person1 = new Person(); // testing Person Class  
             Console.WriteLine(person1.GetAge());
             person1.SetAge(29);
             Console.WriteLine(person1.GetAge());
