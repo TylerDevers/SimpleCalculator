f32c89f16b798d05d77b6ad498c112cd77e5fbd7
Tyler <devers.tyler@gmail.com>
2018-11-13 12:11:56 -0800

    added unit tests for current classes

---
+++ simpleCalculator/simpleCalculator/SimpleCalculator2.Test.Unit/CalculatorEngineTest.cs
@@ -0,0 +1,30 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using simpleCalculator;
+
+namespace SimpleCalculator.Test.Unit
+{
+    [TestClass]
+    public class CalculatorEngineTest
+    {
+        private readonly CalculatorEngine _calculatorEngine = new CalculatorEngine();
+
+        [TestMethod]
+        public void AddsTwoNumbersAndReturnsValidForNonSymbolOperation()
+        {
+            int number1 = 1;
+            int number2 = 2;
+            double result = _calculatorEngine.Calculate("add", number1, number2);
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void AddsTwoNumbersAndReturnsValidForSymbolOperation()
+        {
+            int number1 = 1;
+            int number2 = 2;
+            double result = _calculatorEngine.Calculate("+", number1, number2);
+            Assert.AreEqual(3, result);
+        }
+    }
+}
---
+++ simpleCalculator/simpleCalculator/SimpleCalculator2.Test.Unit/InputConverterTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using simpleCalculator;
+
+namespace SimpleCalculator.Test.Unit
+{
+    [TestClass]
+    public class InputConverterTest
+    {
+        private readonly InputConverter _inputConverter = new InputConverter();
+
+        [TestMethod]
+        public void ConvertsValidStringInputIntoDouble()
+        {
+            string inputNumber = "5";
+            double convertedNumber = _inputConverter.StringToNumber(inputNumber);
+            Assert.AreEqual(5, convertedNumber);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FailsToConvertInvalidStringIntoDouble()
+        {
+            string inputNumber = "*";
+            double convertedNumber = _inputConverter.StringToNumber(inputNumber);
+            Assert.AreEqual(5, convertedNumber);
+        }
+    }
+}
